c6331d90c9b7e13e23fa63c8a1a65ed9dd95e6db
Pavel_Kazlou <pnkazlou@ibagroup.eu>
2024-11-21 22:49:26 +0300

    reinit

--- bot/bot.js
+++
@@ -1,8 +0,0 @@
-const TelegramApi = require("node-telegram-bot-api");
-require('dotenv').config()
-
-const token = process.env.BOT_TOKEN;
-
-const bot = new TelegramApi(token, { polling: true })
-
-module.exports = bot
--- models/TokenModel.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const TokenModel = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: "User" }, //ссылка на айдишник и модель юзера
-  refreshToken: { type: String, required: true },
-});
-
-module.exports = model("Token", TokenModel);
