9ecf639d28c574d554da3e35db24b40f01da0cc5
Pavel_Kazlou <pnkazlou@ibagroup.eu>
2024-11-22 18:51:19 +0300

    fix tests

--- src/common/middlewares/admin.middleware.ts
+++ src/common/middlewares/admin.middleware.ts
@@ -6,19 +6,17 @@ export const adminMiddleware = (req: Request, res: Response, next: NextFunction)
   const auth = req.headers["authorization"] as string;
 
   if (!auth) {
-    console.log("===========ERROR PLACE # 1")
     return next(ApiError.Unauthorized());
   }
   if (auth.slice(0, 6) !== "Basic ") {
-    console.log("===========ERROR PLACE # 2")
-
     return next(ApiError.Unauthorized());
   }
 
   const decodedAuth = fromBase64ToUTF8(auth.slice(6));
 
   if (decodedAuth !== process.env.ADMIN || "") {
-    console.log("===========ERROR PLACE # 3")
+    console.log("===========ERROR PLACE ===============")
+    console.log(process.env.ADMIN)
 
     return next(ApiError.Unauthorized());
     
