814a9046f3c569779e497d2e81da7b8c90f0fd56
Pavel Kazlou <70544122+Pavelkv94@users.noreply.github.com>
2025-03-04 15:46:07 +0300

    Update buildPriorDate.ts

--- src/utils/buildPriorDate.ts
+++ src/utils/buildPriorDate.ts
@@ -1,6 +1,6 @@
 export const buildPriorDate = () => {
   const nowDate = new Date();
-  const priorDate = new Date(nowDate.getTime() - 30 * 24 * 60 * 60 * 1000);
+  const priorDate = new Date(nowDate.getTime() - 28 * 24 * 60 * 60 * 1000);
 
   const year = priorDate.getFullYear();
   const month = String(priorDate.getMonth() + 1).padStart(2, "0");
